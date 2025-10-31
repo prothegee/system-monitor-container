7ac910e162f3682e31a77016e1a0f4ee544f60a3
prothegee <prothegee@proton.me>
2025-10-31 19:24:05 +0700

    checking debug stack 9001

--- stack/backend-9001/build-debug.sh
+++ stack/backend-9001/build-debug.sh
@@ -6,21 +6,17 @@ mkdir -p .public;
 export BUILD_DIR="build"
 export BUILD_TYPE="RelWithDebInfo"
 export BUILD_CXX_FLAGS="-std=c++17"
+export BUILD_CXX_FLAGS_DEBUG="-fsanitize=address;-g"
 
 cmake -S . \
       -G Ninja \
       -B $BUILD_DIR \
       -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
       -DCMAKE_CXX_FLAGS=$BUILD_CXX_FLAGS \
+      -DCMAKE_CXX_FLAGS_DEBUG=$BUILD_CXX_FLAGS_DEBUG \
       -DCMAKE_EXPORT_COMPILE_COMMANDS=1;
 
-if [ -e ./compile_commands.json ]; then
-    rm -rf compile_commands.json;
-fi
-# if [ -e $BUILD_DIR/compile_commands.json ]; then
-#     rm -rf $BUILD_DIR/compile_commands.json;
-# fi
-ln -s $BUILD_DIR/compile_commands.json;
+ln -sf $BUILD_DIR/compile_commands.json ./;
 
 cmake --build build;
 
