5e9805c9d3137be87ed164af9bc4a3a1ccf717f5
prothegee <prothegee@proton.me>
2025-12-22 14:25:14 +0700

    revisit to split

--- dinit.sh
+++ dinit.sh
@@ -3,13 +3,15 @@ set -e;
 
 # internal data directory for each service,
 # where the data used in this local environment.
-mkdir ./service/cassandra/data;
-mkdir ./service/couchbase/data;
-mkdir ./service/grafana/data;
-mkdir ./service/kafka/data;
-mkdir ./service/postgresql/data;
-mkdir ./service/prometheus/data;
-mkdir ./service/rabbitmq/data;
-mkdir ./service/redis/data;
-mkdir ./service/scylladb/data;
+mkdir -p ./service/cassandra/data;
+mkdir -p ./service/couchbase/data;
+mkdir -p ./service/grafana/data;
+mkdir -p ./service/kafka/data;
+mkdir -p ./service/postgresql/data;
+mkdir -p ./service/postgresql/data-postgres-1-cr;
+mkdir -p ./service/postgresql/data-postgres-2-hw;
+mkdir -p ./service/prometheus/data;
+mkdir -p ./service/rabbitmq/data;
+mkdir -p ./service/redis/data;
+mkdir -p ./service/scylladb/data;
 
---
+++ service/postgresql/init.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+set -e;
+
+mkdir -p ./data-postgres-1-cr;
+mkdir -p ./data-postgres-2-hw;
